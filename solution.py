2b8bcd27d70e06199efef1b9d4db0dfe35f30fb7
Kirill <91945571+Kiwatchyou@users.noreply.github.com>
2023-04-13 23:43:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,6 @@ import scipy.stats as stats
 chat_id = 390760498 
 
 
-def solution(x, y) -> bool: 
+def solution(x: np.array, y: np.array) -> bool: 
     
     return stats.ttest_ind(x, y)[1] < 0.03 
